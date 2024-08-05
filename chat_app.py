f2cba511b33c39c9b35d7a5fa5a63b30ff746c96
Mihir Kumar Jha <mihirj@cloudera.com>
2024-08-05 17:37:12 +0530

    Added Changes for UI

--- chat_app.py
+++ chat_app.py
@@ -11,6 +11,7 @@ from utils.cmlllm import (
 from utils.check_dependency import check_gpu_enabled
 import threading
 import itertools
+import shutil
 
 MAX_QUESTIONS = 5
 file_types = ["pdf", "html", "txt"]
@@ -18,8 +19,8 @@ llm_choice = get_supported_models()
 embed_models = get_supported_embed_models()
 lock = threading.Lock()
 
-def save_uploadedfile(uploadedfile):
-    save_dir = "uploaded_files"
+def save_uploadedfile(uploadedfile, collection_name):
+    save_dir = os.path.join("uploaded_files", collection_name)
     if not os.path.exists(save_dir):
         os.makedirs(save_dir)
 
@@ -28,6 +29,21 @@ def save_uploadedfile(uploadedfile):
         f.write(uploadedfile.getbuffer())
 
     return save_path
+
+def delete_collection_name(collection_name):
+    # Remove the corresponding folder
+    collection_dir = os.path.join("uploaded_files", collection_name)
+    if os.path.exists(collection_dir):
+      shutil.rmtree(collection_dir)
+
+def get_collection_folders(directory="uploaded_files"):
+    try:
+        # Get all folders in the specified directory
+        collection_folders = [folder for folder in os.listdir(directory)
+                              if os.path.isdir(os.path.join(directory, folder))]
+        return collection_folders
+    except FileNotFoundError:
+        return []
 
 def reconfigure_llm(model_name="TheBloke/Mistral-7B-Instruct-v0.2-GGUF", embed_model_name="thenlper/gte-large",
                     temperature=0.0, max_new_tokens=1024, context_window=3900, gpu_layers=20):
@@ -67,7 +83,7 @@ def upload_document_and_ingest_new(files, questions, collection_name):
         return "No files to upload"
 
     with ThreadPoolExecutor() as executor:
-        futures = [executor.submit(save_uploadedfile, file) for file in files]
+        futures = [executor.submit(save_uploadedfile, file, collection_name) for file in files]
         saved_files = [future.result() for future in futures]
 
     output = st.session_state.llm.ingest(saved_files, questions, collection_name)
@@ -78,7 +94,9 @@ if 'llm' not in st.session_state:
     with st.spinner('Initializing LLM...'):
         st.session_state.llm = CMLLLM()
 if 'collection_list_items' not in st.session_state:
-    st.session_state.collection_list_items = ["default_collection"]
+    exiting_collection = get_collection_folders()
+    all_collection = list(set(["default_collection"] + exiting_collection))
+    st.session_state.collection_list_items = all_collection
     st.session_state.llm.set_collection_name(collection_name=st.session_state.collection_list_items[0])
 if 'num_questions' not in st.session_state:
     st.session_state.num_questions = 1
@@ -157,6 +175,7 @@ def demo():
                     st.session_state.collection_list_items.remove(collection_name)
                     st.session_state.llm.delete_collection_name(collection_name)
                     st.session_state['success_message'] = f"Collection {collection_name} deleted"
+                    delete_collection_name(collection_name)
                     st.experimental_rerun()
                 elif collection_name == "default_collection":
                     st.error("You can't delete the default collection")
